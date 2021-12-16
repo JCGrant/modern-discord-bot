9a565a2a252f4fa653cc8c46a8b50f28d04d28ee
JCGrant <james@jcgrant.com>
2021-12-16 17:54:46 +0000

    feat load .env on prod

--- src/config.js
+++ src/config.js
@@ -1,11 +1,7 @@
 import dotenv from "dotenv";
+dotenv.config();
 
 export const NODE_ENV = loadEnvVar("NODE_ENV", "development");
-
-if (NODE_ENV !== "production") {
-  dotenv.config();
-}
-
 export const DISCORD_TOKEN = loadEnvVar("DISCORD_TOKEN");
 export const DISCORD_CLIENT_ID = loadEnvVar("DISCORD_CLIENT_ID");
 export const DISCORD_GUILD_ID = loadEnvVar("DISCORD_GUILD_ID");
